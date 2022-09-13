f2e0bb8e908455acab3d095fd878231f48c2b494
BlackPowerade <esam3463@gmail.com>
2022-09-12 20:35:39 -0500

    Unicode fuckery

--- Src/OSD/Logger.cpp
+++ Src/OSD/Logger.cpp
@@ -19,6 +19,13 @@
  ** You should have received a copy of the GNU General Public License along
  ** with Supermodel.  If not, see <http://www.gnu.org/licenses/>.
  **/
+#ifndef UNICODE
+ #undef _UNICODE
+#else
+ #ifndef _UNICODE
+  #define _UNICODE
+ #endif
+#endif
 
 #include "OSD/Logger.h"
 #include <set>
